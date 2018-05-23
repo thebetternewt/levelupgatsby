fa156d906c37910b4fcaf465d507d63308624852
Chris Eady <chris.eady1@gmail.com>
2018-05-23 11:47:05 -0500

    Update pathPrefix for github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'GCP Blog',
     desc: 'A fake blog for GCP built with GatsbyJS',
   },
-  pathPrefix: '/gatsby',
+  pathPrefix: '/levelupgatsby',
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
